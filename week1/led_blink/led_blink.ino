f7eff0bde37b5cfbb773326fcec108ba473b550b
Swarnava Dey <apexe337@gmail.com>
2026-06-28 17:03:19 +0530

    feat: update LED blink interval to 500ms

--- week1/led_blink/led_blink.ino
+++ week1/led_blink/led_blink.ino
@@ -4,7 +4,7 @@ void setup() {
 
 void loop() {
   digitalWrite(13, HIGH);  //LED IS ON
-  delay(1000); //1000ms delay
+  delay(500); //500ms delay
   digitalWrite(13, LOW); //LED IS OFF
-  delay(1000); //1000ms delay
+  delay(500); //500ms delay
 }

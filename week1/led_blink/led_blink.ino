da17c25698f4a34eddc2f294f0f20ec1de2dfdf3
Swarnava Dey <apexe337@gmail.com>
2026-06-28 18:16:59 +0530

    docs: added author name and description of the project

--- week1/led_blink/led_blink.ino
+++ week1/led_blink/led_blink.ino
@@ -1,3 +1,9 @@
+/*
+Author: Swarnava Dey
+Date: 28-06-2026
+Description: Arduino LED blink project with serial output and potentiometer-controlled blink speed.
+*/
+
 int blinkCount = 0; //initialize a variable to count the number of blinks
 int potentioPin = A0; //Potentiometer middle pin is connected to analog pin A0
 

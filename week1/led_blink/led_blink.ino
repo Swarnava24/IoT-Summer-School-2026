ce693e57c0fe0d78d2a2c1e830ba8583c1be49d8
Swarnava Dey <apexe337@gmail.com>
2026-06-28 17:30:00 +0530

    feat: added serial blink counter

--- week1/led_blink/led_blink.ino
+++ week1/led_blink/led_blink.ino
@@ -1,10 +1,18 @@
+int blinkCount = 0; //initialize a variable to count the number of blinks
+
 void setup() {
-  pinMode(13, OUTPUT); //led anode leg is connected to digital pin 13 as output
+  pinMode(13, OUTPUT);
+  Serial.begin(9600); //this mkes a bridge between our laptop and arduino to communicate with a fixed baud rate of 9600
 }
 
 void loop() {
-  digitalWrite(13, HIGH);  //LED IS ON
-  delay(500); //500ms delay
-  digitalWrite(13, LOW); //LED IS OFF
-  delay(500); //500ms delay
+  digitalWrite(13, HIGH);
+  delay(500);
+
+  digitalWrite(13, LOW);
+  delay(500);
+
+  blinkCount++; //update the blinkCount value by 1
+  Serial.print("Blink count: "); //print the message blink count on serial monitor
+  Serial.println(blinkCount); //print the count value and then adds a newline character
 }

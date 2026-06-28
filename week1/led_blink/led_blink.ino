ffd76245db3f1cd1d7a1bf8b3904e2d624ac46ba
Swarnava Dey <apexe337@gmail.com>
2026-06-28 18:11:51 +0530

    feat: control blink speed using potentiometer

--- week1/led_blink/led_blink.ino
+++ week1/led_blink/led_blink.ino
@@ -1,18 +1,21 @@
 int blinkCount = 0; //initialize a variable to count the number of blinks
+int potentioPin = A0; //Potentiometer middle pin is connected to analog pin A0
 
 void setup() {
-  pinMode(13, OUTPUT);
-  Serial.begin(9600); //this mkes a bridge between our laptop and arduino to communicate with a fixed baud rate of 9600
+  pinMode(13, OUTPUT); //led is connected to pin digital pin 13 and the pin is taken as output
+  Serial.begin(9600); //starting the serial monitor with a fixed baud rate of 9600
 }
 
 void loop() {
-  digitalWrite(13, HIGH);
-  delay(500);
+  int delayTime = analogRead(potentioPin); //reading the value of potentiometer and storing it in delayTime variable
 
-  digitalWrite(13, LOW);
-  delay(500);
+  digitalWrite(13, HIGH); //LED BECOMES ON
+  delay(delayTime); //LED is on for delayTime value
 
-  blinkCount++; //update the blinkCount value by 1
-  Serial.print("Blink count: "); //print the message blink count on serial monitor
-  Serial.println(blinkCount); //print the count value and then adds a newline character
+  digitalWrite(13, LOW); //LED BECOMES OFF
+  delay(delayTime); //LED is OFF for delayTime value
+
+  blinkCount++; //updating counting by 1
+  Serial.print("Blink count: "); //print the message
+  Serial.println(blinkCount);
 }

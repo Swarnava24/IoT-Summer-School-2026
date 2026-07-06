a45d6c903f622baf0ee6320b57cd3ea824ef933a
Swarnava Dey <apexe337@gmail.com>
2026-07-06 19:37:37 +0530

    feat: MQTT_Sensor code

--- week4/MQTT_Sensor/MQTT_Sensor.ino
+++ week4/MQTT_Sensor/MQTT_Sensor.ino
@@ -32,7 +32,7 @@ DHTesp dht;
 const int DHT_PIN = 15;
 const int LED_PIN = 2;
 
-// Change YourName to your own name
+
 const char* tempTopic = "iitjammu/summer26/SwarnavaDey/temperature";
 const char* humTopic  = "iitjammu/summer26/SwarnavaDey/humidity";
 const char* ledTopic  = "iitjammu/summer26/SwarnavaDey/led_control";
